271a75a8f7707d7e1cc486a711e8078dc507282b
ScottWallin <scott.wallin@me.com>
2023-06-09 16:46:07 -0600

    logo

--- tower.client/src/components/Navbar.vue
+++ tower.client/src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-3">
     <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
       <div class="d-flex flex-column align-items-center">
-        <img alt="logo" src="../assets/img/cw-logo.png" height="45" />
+        <img alt="logo" src="../assets/img/Logo.svg" height="45" />
       </div>
     </router-link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
@@ -36,6 +36,9 @@ export default {
 </script>
 
 <style scoped>
+nav{
+  color: #685044;
+}
 a:hover {
   text-decoration: none;
 }
